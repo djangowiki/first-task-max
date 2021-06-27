6e53da84920c2fecff0ac00dc9817fe1f3c3caff
Ikenna Gabriel Ifenna <13273983+djangowiki@users.noreply.github.com>
2021-06-27 09:19:05 +0100

    Mongo Error Fix for heroku

--- config/db.js
+++ config/db.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose');
 
 // Connect Database Function.
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  });
-  console.log(`MongoDB Connected on the server ${conn.connection.host}`);
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log(`MongoDB Connected on the server ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports = connectDB;
